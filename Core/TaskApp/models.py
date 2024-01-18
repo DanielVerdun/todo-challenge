f0c0cb1aefbf9db14e7346c9317bf90e2c3238fc
Daniel Verdun <verdundaniel20@gmail.com>
2024-01-17 22:35:15 -0300

    Creamos requeriments.txt, instalamos DRF, creamos models.py y tambien serializers.py

--- Core/TaskApp/models.py
+++ Core/TaskApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Cramos el modelo Task : El cual se encargará de almacenar las datos cada una de las tareas
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
